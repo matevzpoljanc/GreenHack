4dfb75578a39a9f3a329bd0cdd03e2d08772241f
Matevz Poljanc <matevz.poljanc@gmail.com>
2018-03-10 17:54:08 +0000

    Update server

--- server.py
+++ server.py
@@ -3,13 +3,19 @@ from flask import request
 from flask import jsonify
 import random
 
+from offTomorrow import off_tomorrow
+
 app = Flask(__name__)
 
 @app.route("/predict")
 def predict():
     days_tolerated = int(request.values['days'])
-    max_temperature = int(request.values['temperature'])
-    out = {"tolerated_days":days_tolerated, "max_tolerated_temperature":max_temperature, "prediction":"Some fancy stuff :D"}
+    max_temperature = int(request.values['maxTemperature'])
+    desired_temperature = int(request.values['desiredTemperature'])
+    out = {"tolerated_days": days_tolerated,
+           "max_tolerated_temperature": max_temperature,
+           "desired_temperature": desired_temperature,
+           "prediction": off_tomorrow(desired_temperature, max_temperature, days_tolerated)}
     return jsonify(out)
 
 
@@ -17,8 +23,9 @@ def predict():
 @app.route("/demo")
 def demo():
     days_tolerated = int(request.values['days'])
-    max_temperature = int(request.values['temperature'])
-    out = {"tolerated_days": 1, "max_tolerated_temperature": 2, "prediction": 0}
+    max_temperature = int(request.values['maxTemperature'])
+    desired_temperature = int(request.values['desiredTemperature'])
+    out = {"tolerated_days": days_tolerated, "max_tolerated_temperature": max_temperature, "desired_temperature": desired_temperature, "prediction": 0}
     bias = 0.7
     if random.random() > bias:
         out["prediction"] = 1
