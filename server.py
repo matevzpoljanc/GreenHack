ada5681e41bb9eea944adfa3d2aac106c80a616a
Matevz Poljanc <matevz.poljanc@gmail.com>
2018-03-10 16:58:40 +0000

    Introduce demo endpoint

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+import random
 
 app = Flask(__name__)
 
@@ -9,6 +10,18 @@ def predict():
     days_tolerated = int(request.values['days'])
     max_temperature = int(request.values['temperature'])
     out = {"tolerated_days":days_tolerated, "max_tolerated_temperature":max_temperature, "prediction":"Some fancy stuff :D"}
+    return jsonify(out)
+
+
+
+@app.route("/demo")
+def demo():
+    days_tolerated = int(request.values['days'])
+    max_temperature = int(request.values['temperature'])
+    out = {"tolerated_days": 1, "max_tolerated_temperature": 2, "prediction": 0}
+    bias = 0.7
+    if random.random() > bias:
+        out["prediction"] = 1
     return jsonify(out)
 
 if __name__ == '__main__':
